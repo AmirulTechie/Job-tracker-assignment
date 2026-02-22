aa4c34385a8c838fc9d986c7cba681df73d729a1
Md. Amirul Islam <amirulislam.office28@gmail.com>
2026-02-22 21:32:38 +0600

    all card added successfully. added font awsome cdn

---
+++ script.js
@@ -0,0 +1,5 @@
+let totalCount = document.getElementById('total-count');
+let interviewCount = document.getElementById('interview-count');
+let rejectedCount = document.getElementById('rejected-count');
+
+console.log(totalCount);
